aa59a324446c263f93c1a315adbfc412492381c2
lightnonstop <umichaeledet003@gmail.com>
2023-06-06 15:42:39 +0100

    renamed a jsx file to begin with uppercase

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import OurStore from './pages/OurStore';
 function App() {
   return (
     <>
@@ -12,6 +13,7 @@ function App() {
             <Route index element={<Home />} />
             <Route path='about' element={<About />} />
             <Route path='contact' element={<Contact />} />
+            <Route path='store' element={<OurStore />} />
           </Route>
         </Routes>
       </Router>
---
+++ frontend/src/pages/OurStore.tsx
@@ -0,0 +1,5 @@
+export default function OurStore() {
+  return (
+    <div>OurStore</div>
+  )
+}
