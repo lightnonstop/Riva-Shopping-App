910b213ff7b62661f30b3f5e6d5865b3f2d99ca5
lightnonstop <umichaeledet003@gmail.com>
2023-06-06 12:31:51 +0100

    remove class name of wishlist-right

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import ReactStars from 'react-rating-stars-component';
+import { Link } from 'react-router-dom';
 export default function ProductCard() {
   return (
     <div className='col-3'>
-      <div className='product-card'>
+      <div className='product-card position-relative'>
+        <div className='wishlist-icon position-absolute'>
+          <Link to=''>
+            <img src="/images/wish.svg" alt="wishlist" />
+          </Link>
+        </div>
         <div className='product-image'>
           <img src="/images/watch.jpg" alt="product" />
         </div>
@@ -12,9 +18,29 @@ export default function ProductCard() {
           <h5 className='product-title'>
             Kids headphones coloured for students
           </h5>
+          <ReactStars
+            count={5}
+            size={24}
+            value='3'
+            edit={false}
+            activeColor='#FFD700'
+           />
           <p className="price">$100.00</p>
+        </div>
+        <div className='action-bar position-absolute'>
+          <div className='d-flex flex-column gap-15'>
+            <Link to=''>
+              <img src="/images/prodcompare.svg" alt="compare" />
+            </Link>
+            <Link to=''>
+              <img src="/images/view.svg" alt="view" />
+            </Link>
+            <Link to=''>
+              <img src="/images/add-cart.svg" alt="add-cart" />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
   )
-}
+} 
