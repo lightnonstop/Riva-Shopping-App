ed1fd4b1132e07ab01a626115b6821b90d7f4415
lightnonstop <umichaeledet003@gmail.com>
2023-06-07 12:17:17 +0100

    align button and link at the center

---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+import { Link } from 'react-router-dom';
+export default function Login() {
+  return (
+    <>
+        <Meta title="Login" />
+        <BreadCrumb title="Login" />
+        <div className='login-wrapper home-wrapper-2 py-5'>
+            <div className='row'>
+                <div className='col-12'>
+                    <div className="login-card">
+                        <h3>Login</h3>
+                        <form action="">
+                            <div>
+                                <input type="email" name='email' className="form-control" placeholder='email' />
+                            </div>
+
+                            <div>
+                                <input type="password" name='password' className="form-control" placeholder='password' />
+                            </div>
+                            <div>
+                                <Link to='/forgot-password'>Forgot Password</Link>
+                                <div className='d-flex justify-content-center align-items-center gap-15'>
+                                    <button className='button border-0'>Login</button>
+                                    <Link to='/signup' className='button border-0'>Signup</Link>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
