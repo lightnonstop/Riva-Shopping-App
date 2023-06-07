bcb6ed913c0a0626e238595273759223449efd39
lightnonstop <umichaeledet003@gmail.com>
2023-06-07 11:40:27 +0100

    add color component

---
+++ client/src/components/Color.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Color() {
+    return (
+        <ul className="colors ps-0">
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    )
+}
