83c5cf6b3f5726637e9ec8e528b5622aea8b689b
lightnonstop <umichaeledet003@gmail.com>
2023-06-07 08:46:18 +0100

    remove wrapping container

---
+++ frontend/src/components/BlogCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function BlogCard() {
+  return (
+    <div className='blog-card'>
+      <div className='card-image'>
+        <img src="/images/blog-1.jpg" className='img-fluid' alt="blog" />
+      </div>
+      <div className='blog-content'>
+        <p className='date'>29 May, 2023</p>
+        <h5 className='title'>A beautiful sunday morning renaissance</h5>
+        <p className='desc'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, mollitia! Exercitationem
+        </p>
+        <Link to='' className="button">Read More</Link>
+      </div>
+    </div>
+  )
+}
