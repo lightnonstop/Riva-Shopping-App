4178d1bab4509b699c81c96eae44bb854b80e1bf
lightnonstop <umichaeledet003@gmail.com>
2023-06-07 11:48:22 +0100

    add wishlist component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import Contact from './pages/Contact';
 import OurStore from './pages/OurStore';
 import Blog from './pages/Blog';
 import CompareProduct from './pages/CompareProduct';
+import Wishlist from './pages/Wishlist';
 function App() {
   return (
     <>
@@ -18,6 +19,7 @@ function App() {
             <Route path='store' element={<OurStore />} />
             <Route path='blogs' element={<Blog />} />
             <Route path='compare-product' element={<CompareProduct />} />
+            <Route path='wishlist' element={<Wishlist />} />
           </Route>
         </Routes>
       </Router>
