ec944c42f02a9e87b00f86bb5fb761a94d69d112
lightnonstop <umichaeledet003@gmail.com>
2023-06-07 09:29:52 +0100

    blog card image take full width of container

--- client/src/components/BlogCard.tsx
+++ client/src/components/BlogCard.tsx
@@ -5,7 +5,7 @@ export default function BlogCard() {
   return (
     <div className='blog-card'>
       <div className='card-image'>
-        <img src="/images/blog-1.jpg" className='img-fluid' alt="blog" />
+        <img src="/images/blog-1.jpg" className='img-fluid w-100' alt="blog" />
       </div>
       <div className='blog-content'>
         <p className='date'>29 May, 2023</p>
