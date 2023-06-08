e7f175e31292879ee847121821ade957c775679d
lightnonstop <umichaeledet003@gmail.com>
2023-06-08 23:23:49 +0100

    add shopping app image

---
+++ client/src/images/index.tsx
@@ -0,0 +1,20 @@
+import { default as prodcompare } from '../images/prodcompare.svg';
+import { default as wish } from '../images/wish.svg';
+import { default as wishlist } from '../images/wishlist.svg';
+import { default as watch } from '../images/watch.jpg';
+import { default as watch2 } from '../images/watch-1.avif';
+import { default as addcart } from '../images/add-cart.svg';
+import { default as view } from '../images/view.svg';
+import { default as newsletter } from '../images/newsletter.png';
+
+
+export {
+    prodcompare,
+    wish,
+    wishlist,
+    watch,
+    watch2,
+    addcart,
+    view,
+    newsletter,
+}
