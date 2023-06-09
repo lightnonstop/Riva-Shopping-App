2d2b3705a0c9b3ff1124e00cae5ee9c0c3d4bd5f
lightnonstop <umichaeledet003@gmail.com>
2023-06-09 08:46:38 +0100

    form refactor to use Input component

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,25 +2,21 @@ import React from 'react'
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import { Link } from 'react-router-dom';
+import Container from '../components/Container';
+import Input from '../components/Input';
 export default function Login() {
-  return (
-    <>
-        <Meta title="Login" />
-        <BreadCrumb title="Login" />
-        <div className='login-wrapper home-wrapper-2 py-5'>
-            <div className='container-xxl'>
+    return (
+        <>
+            <Meta title="Login" />
+            <BreadCrumb title="Login" />
+            <Container containerClass='login-wrapper home-wrapper-2 py-5'>
                 <div className='row'>
                     <div className='col-12'>
                         <div className="auth-card">
                             <h3 className='text-center mb-3'>Login</h3>
                             <form action="" className='d-flex flex-column gap-15'>
-                                <div>
-                                    <input type="email" name='email' className="form-control" placeholder='Email' />
-                                </div>
-
-                                <div className='mt-1'>
-                                    <input type="password" name='password' className="form-control" placeholder='Password' />
-                                </div>
+                                <Input type="email" name='email' placeholder='Email' />
+                                <Input type="password" name='password' placeholder='Password' />
                                 <div>
                                     <Link className='text-decoration-underline' to='/forgot-password'>Forgot Password?</Link>
                                     <div className='mt-3 d-flex justify-content-center align-items-center gap-15'>
@@ -32,8 +28,7 @@ export default function Login() {
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </>
-  )
+            </Container>
+        </>
+    )
 }
