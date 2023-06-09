c0b214188d3a116264027f49097572cb159ed6af
lightnonstop <umichaeledet003@gmail.com>
2023-06-09 08:42:36 +0100

    refactor code to use Input component

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import Container from '../components/Container';
+import Input from '../components/Input';
 export default function Signup() {
     return (
         <>
@@ -13,21 +14,10 @@ export default function Signup() {
                         <div className="auth-card">
                             <h3 className='text-center mb-3'>Signup</h3>
                             <form action="" className='d-flex flex-column gap-15'>
-                                <div>
-                                    <input type="text" name='name' className="form-control" placeholder='Name' />
-                                </div>
-
-                                <div>
-                                    <input type="email" name='email' className="form-control" placeholder='Email' />
-                                </div>
-
-                                <div>
-                                    <input type="tel" name='mobile' className="form-control" placeholder='Mobile Number' />
-                                </div>
-
-                                <div className='mt-1'>
-                                    <input type="password" name='password' className="form-control" placeholder='Password' />
-                                </div>
+                                <Input type='text' name='name' placeholder='Name' />
+                                <Input type='email' name='email' placeholder='Email' />
+                                <Input type='tel' name='mobile' placeholder='Mobile Number' />
+                                <Input type='password' name='password' placeholder='password' />
                                 <div>
                                     <div className='mt-3 d-flex justify-content-center align-items-center gap-15'>
                                         <button className='button border-0'>Signup</button>
