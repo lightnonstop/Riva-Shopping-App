1fc76d4b8eba8d926cf52d53ff96df8cf752f030
lightnonstop <umichaeledet003@gmail.com>
2023-06-09 08:54:56 +0100

    form refactor to use Input Component

--- client/src/pages/ResetPassword.tsx
+++ client/src/pages/ResetPassword.tsx
@@ -1,26 +1,21 @@
 import React from 'react'
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
+import Container from '../components/Container';
+import Input from '../components/Input';
 export default function ResetPassword() {
-  return (
-    <>
-        <Meta title="Reset Password" />
-        <BreadCrumb title="Reset Password" />
-        <div className='login-wrapper home-wrapper-2 py-5'>
-            <div className='container-xxl'>
+    return (
+        <>
+            <Meta title="Reset Password" />
+            <BreadCrumb title="Reset Password" />
+            <Container containerClass='login-wrapper home-wrapper-2 py-5'>
                 <div className='row'>
                     <div className='col-12'>
                         <div className="auth-card">
                             <h3 className='text-center mb-3'>Reset Password</h3>
                             <form action="" className='d-flex flex-column gap-15'>
-                                <div className='mt-1'>
-                                    <input type="password" name='password' className="form-control" placeholder='Password' />
-                                </div>
-                                
-                                <div className='mt-1'>
-                                    <input type="password" name='confirm-password' className="form-control" placeholder='Confirm Password' />
-                                </div>
-
+                                <Input type="password" name='password' placeholder='Password' />
+                                <Input type="password" name='confirm-password' placeholder='Confirm Password' />
                                 <div>
                                     <div className='mt-3 d-flex justify-content-center align-items-center gap-15'>
                                         <button className='button border-0'>Done</button>
@@ -30,8 +25,7 @@ export default function ResetPassword() {
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </>
-  )
+            </Container>
+        </>
+    )
 }
