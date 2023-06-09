e7e6f15490050dae9db3adf51ce9a2eee6b26a75
lightnonstop <umichaeledet003@gmail.com>
2023-06-09 07:37:24 +0100

    remove outside extra large container and refactor code

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,23 +1,22 @@
 import React from 'react'
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
-import { Link } from 'react-router-dom';
+import Container from '../components/Container';
 export default function Signup() {
-  return (
-    <>
-        <Meta title="Signup" />
-        <BreadCrumb title="Signup" />
-        <div className='container-xxl'>
-            <div className='login-wrapper home-wrapper-2 py-5'>
+    return (
+        <>
+            <Meta title="Signup" />
+            <BreadCrumb title="Signup" />
+            <Container containerClass='login-wrapper home-wrapper-2 py-5'>
                 <div className='row'>
                     <div className='col-12'>
                         <div className="auth-card">
-                            <h3 className='text-center mb-3'>Login</h3>
+                            <h3 className='text-center mb-3'>Signup</h3>
                             <form action="" className='d-flex flex-column gap-15'>
                                 <div>
                                     <input type="text" name='name' className="form-control" placeholder='Name' />
                                 </div>
-                                
+
                                 <div>
                                     <input type="email" name='email' className="form-control" placeholder='Email' />
                                 </div>
@@ -38,8 +37,7 @@ export default function Signup() {
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </>
-  )
+            </Container>
+        </>
+    )
 }
