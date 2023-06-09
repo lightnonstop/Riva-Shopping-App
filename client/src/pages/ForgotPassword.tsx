d2dae7d0db128112e9a05bce391bb354ec49ab3d
lightnonstop <umichaeledet003@gmail.com>
2023-06-09 08:51:10 +0100

    form refactor to use Input component

--- client/src/pages/ForgotPassword.tsx
+++ client/src/pages/ForgotPassword.tsx
@@ -2,22 +2,21 @@ import React from 'react'
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import { Link } from 'react-router-dom';
+import Container from '../components/Container';
+import Input from '../components/Input';
 export default function ForgotPassword() {
-  return (
-    <>
-        <Meta title="Forgot Password" />
-        <BreadCrumb title="Forgot Password" />
-        <div className='login-wrapper home-wrapper-2 py-5'>
-            <div className='container-xxl'>
+    return (
+        <>
+            <Meta title="Forgot Password" />
+            <BreadCrumb title="Forgot Password" />
+            <Container containerClass='login-wrapper home-wrapper-2 py-5'>
                 <div className='row'>
                     <div className='col-12'>
                         <div className="auth-card">
-                            <h3 className='text-center mb-3'>Reset Your Password</h3>
+                            <h3 className='text-center mb-3'>Forgot Your Password</h3>
                             <p className='text-center mt-2 mb-3'>We will send you an email to reset your password</p>
                             <form action="" className='d-flex flex-column gap-15'>
-                                <div>
-                                    <input type="email" name='email' className="form-control" placeholder='Email' />
-                                </div>
+                                <Input type="email" name='email' placeholder='Email' />
                                 <div>
                                     <div className='mt-3 d-flex flex-column justify-content-center align-items-center gap-15'>
                                         <button className='button border-0' type='submit'>Submit</button>
@@ -28,8 +27,7 @@ export default function ForgotPassword() {
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </>
-  )
+            </Container>
+        </>
+    )
 }
