79676841d15d806825193877f6a3e6e42de20872
lightnonstop <umichaeledet003@gmail.com>
2023-06-09 07:26:55 +0100

    refactor code to use Container component

--- client/src/pages/Checkout.tsx
+++ client/src/pages/Checkout.tsx
@@ -1,33 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { BiArrowBack } from 'react-icons/bi'
+import { watch } from '../images'
+import Container from '../components/Container'
 export default function Checkout() {
-  return (
-    <>
-        <div className="checkout-wrapper py-5 home-wrapper-2">
-            <div className="container-xxl">
+    return (
+        <>
+            <Container containerClass="checkout-wrapper py-5 home-wrapper-2">
                 <div className="row">
                     <div className="col-7">
                         <div className="checkout-left-data">
                             <h3 className='website-name'>Riva</h3>
                             <nav
-                            aria-label='breadcrumb'
+                                aria-label='breadcrumb'
                             >
                                 <ol className='breadcrumb'>
                                     <li className='breadcrumb-item'>
-                                        <Link className='text-dark' to="/cart">Cart</Link>
+                                        <Link className='text-dark total-price' to="/cart">Cart</Link>
                                     </li>
-                                    <li className='breadcrumb-item active' aria-current='page'>Information</li>
-                                    <li className='breadcrumb-item'>
+                                    <li className='breadcrumb-item total-price active' aria-current='page'>Information</li>
+                                    <li className='breadcrumb-item total-price'>
                                         Shipping
                                     </li>
                                     <li className='breadcrumb-item active' aria-current='page'>Payment</li>
                                 </ol>
                             </nav>
-                            <h4 className='title'>Contact Information</h4>
-                            <p className='user-details'>
+                            <h4 className='title total'>Contact Information</h4>
+                            <p className='user-details total'>
                                 Unwana Michael (umichaeledet003@gmail.com)
                             </p>
+                            <h4 className='mb-3'>Shipping Address</h4>
                             <form action="" className='d-flex flex-wrap gap-15 justify-content-between'>
                                 <div className='w-100'>
                                     <select name="" id="" className='form-control form-select'>
@@ -71,21 +73,40 @@ export default function Checkout() {
                         </div>
                     </div>
                     <div className="col-5">
-                        <div></div>
-                        <div>
-                            <div className='d-flex justify-content-between align-items-center'>
-                                <p>Total</p>
-                                <p>$ 10000</p>
+                        <div className='border-bottom py-4'>
+                            <div className='d-flex gap-10 align-items-center'>
+                                <div className='w-75 d-flex mb-2 gap-10'>
+                                    <div className='w-25 position-relative'>
+                                        <span style={{ top: '-10px', right: '-5px' }} className='badge bg-secondary text-white rounded-circle w-25 h-25 position-absolute d-flex align-items-center justify-content-center'>1</span>
+                                        <img className='img-fluid' src={watch} alt="watch" />
+                                    </div>
+                                    <div>
+                                        <h5 className='total-price'>grfgrr</h5>
+                                        <p className='total-price'>s / #fggg</p>
+                                    </div>
+                                </div>
+                                <div className='flex-grow-1'>
+                                    <h5 className='total'>$ 100</h5>
+                                </div>
                             </div>
                         </div>
-                        <div className='d-flex justify-content-between align-items-center'>
-                            <h4>Total</h4>
-                            <h5>$ 10000</h5>
+                        <div className='border-bottom py-4'>
+                            <div className='d-flex justify-content-between align-items-center'>
+                                <p className='mb-0 total'>SubTotal</p>
+                                <p className='mb-0 total-price'>$ 10000</p>
+                            </div>
+                            <div className='d-flex justify-content-between align-items-center'>
+                                <p className='total'>Shipping</p>
+                                <p className='total-price'>$ 10000</p>
+                            </div>
+                        </div>
+                        <div className='d-flex justify-content-between align-items-center border-bottom py-4'>
+                            <h4 className='total'>Total</h4>
+                            <h5 className='total-price'>$ 10000</h5>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </>
-  )
+            </Container>
+        </>
+    )
 }
