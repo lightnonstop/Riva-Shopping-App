b756623b2cb9ac49e4b0166b75e30a70157479c5
lightnonstop <umichaeledet003@gmail.com>
2023-06-20 08:59:51 +0100

    toast container

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,26 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 export default function Layout() {
   return (
     <>
         <Header />
         <Outlet />
         <Footer />
+        <ToastContainer 
+          position={"top-right"}
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
     </>
   )
 }
