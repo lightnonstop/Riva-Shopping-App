649391367f35249e4c71004298717699c68cb9d1
lightnonstop <umichaeledet003@gmail.com>
2023-06-20 08:59:35 +0100

    onChange and onBlur for input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import { ChangeEvent } from 'react'
 interface InputProps {
     type: string;
     name: string;
     placeholder: string;
     className?: string;
+    value: string; 
+    onChange: (arg: string | ChangeEvent) => void;
+    onBlur: (arg: string | ChangeEvent) => void;
 }
-export default function Input({ type, name, placeholder, className }: InputProps) {
+export default function Input({ type, name, placeholder, className, value, onChange, onBlur, }: InputProps) {
   return (
     <div>
         <input type={type} name={name} placeholder={placeholder} className={className 
         ? `form-control ${className}`
         : 'form-control'
-      } />
+      } value={value} onChange={onChange} onBlur={onBlur} />
     </div>
   );
 }
