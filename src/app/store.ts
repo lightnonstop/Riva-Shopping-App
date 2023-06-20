79124b08ea70d829bd1f868c86ca32a739efa2ba
lightnonstop <umichaeledet003@gmail.com>
2023-06-20 14:40:58 +0100

    add new, blog reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import authReducer from '../features/user/userSlice';
 import productReducer from '../features/product/productSlice';
+import blogReducer from '../features/blog/blogSlice';
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     product: productReducer,
+    blog: blogReducer,
   },
 });
 console.log(store.getState());
