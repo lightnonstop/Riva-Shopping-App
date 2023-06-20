8725c2574769cf8016efd8ea49409296e908c221
lightnonstop <umichaeledet003@gmail.com>
2023-06-20 08:58:43 +0100

    configure token from local storage

---
+++ src/utils/axiosConfig.ts
@@ -0,0 +1,10 @@
+export const baseUrl = 'http://localhost:4000/api/';
+
+const getTokenFromLocalStorage = localStorage.getItem('customer') ? JSON.parse(localStorage.getItem('customer')!) : "";
+
+export const config = {
+    headers: {
+        Authorization: `Bearer ${getTokenFromLocalStorage.token}`,
+        Accept: 'application/json',
+    },
+}
