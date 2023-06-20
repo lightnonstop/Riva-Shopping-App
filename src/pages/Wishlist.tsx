8c03388095bcdad4b811f0d61f248b3b70fa4208
lightnonstop <umichaeledet003@gmail.com>
2023-06-20 09:24:23 +0100

    fix wishlist is undefined

--- src/pages/Wishlist.tsx
+++ src/pages/Wishlist.tsx
@@ -1,56 +1,52 @@
-import React from 'react'
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import Container from '../components/Container';
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { getUserWishlist } from "../features/user/userSlice";
+import { useEffect } from "react";
+import { addProductToWishlist } from "../features/product/productSlice";
 export default function Wishlist() {
+    const dispatch = useAppDispatch();
+    const wishlist = useAppSelector(state => state.auth.wishlist?.wishlist);
+
+    useEffect(() => {
+        dispatch(getUserWishlist())
+    }, [])
+    const removeFromWishlist = (id: { prodId: string; }) => {
+        dispatch(addProductToWishlist(id))
+        setTimeout(() => {
+            dispatch(getUserWishlist())
+        },0)
+      }
     return (
         <>
             <Meta title="Wishlist" />
             <BreadCrumb title="Wishlist" />
             <Container containerClass='wishlist-wrapper home-wrapper-2 py-5'>
-                <div className='row'>
-                    <div className='col-3'>
-                        <div className='wishlist-card position-relative'>
-                            <img src="/images/cross.svg" className='position-absolute cross img-fluid' alt="cross" />
-                            <div className='wishlist-card-image'>
-                                <img src="/images/watch.jpg" className='img-fluid w-100' alt="watch" />
-                            </div>
-                            <div className='p-3'>
-                                <h5 className='title'>
-                                    Honor T1 7.0 RAM  8 GB ROM 7 Inch with Wi-Fi + 3G Tablet
-                                </h5>
-                                <h6 className='price my-3'>$ 100</h6>
-                            </div>
+                <div className='row text-center fs-3'>
+                    {wishlist?.length === 0 && (
+                        <div>
+                            No data
                         </div>
-                    </div>
-                    <div className='col-3'>
-                        <div className='wishlist-card position-relative'>
-                            <img src="/images/cross.svg" className='position-absolute cross img-fluid' alt="cross" />
-                            <div className='wishlist-card-image'>
-                                <img src="/images/watch.jpg" className='img-fluid w-100' alt="watch" />
+                    )}
+                    {
+                        wishlist?.map((product, index: string) => (
+                            <div key={index} className='col-3'>
+                                <div className='wishlist-card position-relative'>
+                                    <img onClick={() => removeFromWishlist({ prodId: product._id })} src="/images/cross.svg" className='position-absolute cross img-fluid' alt="cross" />
+                                    <div className='wishlist-card-image'>
+                                        <img src={product.images[0] ? product.images[0] :  "/images/watch.jpg"} className='img-fluid w-100' alt="watch" />
+                                    </div>
+                                    <div className='p-3'>
+                                        <h5 className='title'>
+                                            {product.title}
+                                        </h5>
+                                        <h6 className='price my-3'>{`$ ${product.price}`}</h6>
+                                    </div>
+                                </div>
                             </div>
-                            <div className='p-3'>
-                                <h5 className='title'>
-                                    Honor T1 7.0 RAM  8 GB ROM 7 Inch with Wi-Fi + 3G Tablet
-                                </h5>
-                                <h6 className='price my-3'>$ 100</h6>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-3'>
-                        <div className='wishlist-card position-relative'>
-                            <img src="/images/cross.svg" className='position-absolute cross img-fluid' alt="cross" />
-                            <div className='wishlist-card-image'>
-                                <img src="/images/watch.jpg" className='img-fluid w-100' alt="watch" />
-                            </div>
-                            <div className='p-3'>
-                                <h5 className='title'>
-                                    Honor T1 7.0 RAM  8 GB ROM 7 Inch with Wi-Fi + 3G Tablet
-                                </h5>
-                                <h6 className='price my-3'>$ 100</h6>
-                            </div>
-                        </div>
-                    </div>
+                        ))
+                    }
                 </div>
             </Container>
         </>
