08ab4e67efba4375608fa5e8fcbf8d13ccdc4be2
lightnonstop <umichaeledet003@gmail.com>
2023-06-21 07:09:43 +0100

    add unique key prop fro child elements

--- src/components/Color.tsx
+++ src/components/Color.tsx
@@ -5,8 +5,8 @@ export default function Color({ data }: ColorProps) {
     return (
         <ul className="colors ps-0">
             {
-                data && data.map(color => (
-                    <li style={{ backgroundColor: color.title }}></li>
+                data && data.map((color, index) => (
+                    <li key={index} style={{ backgroundColor: color.title }}></li>
                 ))
             }
         </ul>
